5bf95be81f57442d526f3edc082a67937b7273a1
kimmolepola <kimmo.lepola@gmail.com>
2022-07-10 17:46:46 +0300

    hello

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const path = require('path');
 const express = require('express');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, './build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './build', 'index.html'));
+// });
+
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`); // eslint-disable-line
